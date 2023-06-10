e51584f8a8f2cb5eecf288fb343ce2fe0e325186
Rohit Raj <133978902+Rohit-Raj1845@users.noreply.github.com>
2023-06-11 02:47:44 +0530

    Add files via upload

---
+++ oops_assignment_1.py
@@ -0,0 +1,28 @@
+QUESTION:-1. Explain class and object with respect to object-oriented programming. Give a suitable example.
+Classes are user-defined data types that act as the blueprint for individual objects, attributes and methods. Objects are instances of a class created with specifically defined data.
+Example:- A dog has states-colors, name, breed as well as behaviours-wagging the tail, barking, eating. 
+
+
+
+QUESTION:-2. Name the four pillars of oops.
+1. Abstraction.
+2. Encapsulation.
+3. Inheritance.
+4. Polymorphism.
+
+
+
+QUESTION:-3. Explain why the __init__() function is used.Give a suitable example.
+These types of functions are used to initialize the attributes of that class.
+Example:- If the class is person, then the attributes will be name, age, etc.
+
+
+
+QUESTION:-4. Why self is used in oops?
+The self variable is used to represent the instance of the class which is often used in object-oriented programming. It works as a reference to the object.
+
+
+
+QUESTION:-5. What is inheritance? Give an example for each type of inheritance.
+Inheritance is a mechanism in which one class acquires the property of another class.
+For Example:- A child inherits the traits of his/her parents.
